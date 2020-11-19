1f631acb5a1eb112ab05624dec9683af90572e7c
Jonathan Gonzalez <jgonzalez249@gmail.com>
2020-11-18 23:26:16 -0500

    Initial commit

---
+++ InventoryManager.WinForm/ViewModels/WorldViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManager.Data;
+using System.ComponentModel;
+
+namespace InventoryManager.WinForm.ViewModels
+{
+    public class WorldViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Filename { get; set; }
+     
+        public World World { get; set; }
+    }
+}
